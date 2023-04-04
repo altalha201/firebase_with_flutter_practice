7f678db4e27a4d7712b5bb480fe799421bbac751
Md. Motasaddique Al Talha <67387060+altalha201@users.noreply.github.com>
2023-04-05 00:13:01 +0600

    Get data from firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const FirebaseApp());
 }
 
@@ -11,25 +17,73 @@ class FirebaseApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "Flutter with Firebase",
-      home: HomePage(),
+      home: BookListScreen(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class BookListScreen extends StatefulWidget {
+  const BookListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BookListScreen> createState() => _BookListScreenState();
+}
+
+class _BookListScreenState extends State<BookListScreen> {
+
+  FirebaseFirestore fireStore = FirebaseFirestore.instance;
+  List<Book> books = [];
+  bool gettingBooks = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getAllBooks();
+  }
+  
+  Future<void> getAllBooks() async {
+    setState(() {
+      gettingBooks = true;
+    });
+    books.clear();
+    await fireStore.collection('Books').get().then((documents) {
+      log(documents.docs.length.toString());
+      for (var doc in documents.docs) {
+        books.add(Book(doc.get('name'), doc.get('writer'), doc.get('year')));
+      }
+    });
+    setState(() {
+      gettingBooks = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Firebase Application"),
+        title: const Text("Book List"),
       ),
-      body: const Center(
-        child: Text("New app"),
-      ),
+      body: gettingBooks
+          ? const Center(child: CircularProgressIndicator(),)
+          : ListView.builder(
+          itemCount: books.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(books[index].name),
+              subtitle: Text(books[index].authorName),
+              trailing: Text(books[index].year),
+            );
+          }
+      )
     );
   }
 }
+
+class Book {
+  final String name, authorName, year;
+
+  Book(this.name, this.authorName, this.year);
+}
+
 
 
